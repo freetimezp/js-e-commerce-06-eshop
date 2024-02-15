a7c44682ae144375963ff3ee0ba9bf552ed1ce2c
freetimezp <kraftukrnet@ukr.net>
2024-02-15 21:58:13 +0200

    05. js-e-commerce-06-eshop. Hero Component.

---
+++ frontend/src/components/Shared/Button.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+const Button = ({ text, bgColor, textColor, handler = () => { } }) => {
+    return (
+        <button className={`${bgColor} ${textColor} cursor-pointer hover:scale-105 
+            duration-300 transition-all py-2 px-8 rounded-3xl font-semibold relative 
+            z-10`}
+            onClick={handler}
+        >
+            {text}
+        </button>
+    );
+}
+
+export default Button;
