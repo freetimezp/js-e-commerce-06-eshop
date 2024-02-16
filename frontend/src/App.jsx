390dbdc23632b2f0fddb72e7fd9ce1d54cc5973a
freetimezp <kraftukrnet@ukr.net>
2024-02-16 17:54:22 +0200

    07. js-e-commerce-06-eshop. Services Component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-800 dark:text-white duration-200 transition-all
+      overflow-hidden">
       <Navbar />
       <Hero />
       <Category />
       <Category2 />
+      <Services />
     </div>
   )
 }
